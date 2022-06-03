c104bbfbacd15af671d24a63e93407aa2b6c4227
Hector Martinez <elcuatloo44@gmail.com>
2022-06-03 11:34:35 -0500

    Agregar archivos de proyecto.

---
+++ ROT13/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿
+namespace ROT13
+{
+    partial class ROT13
+    {
+        /// Variable del diseñador necesaria.
+        private System.ComponentModel.IContainer components = null;
+
+        /// Limpiar los recursos que se estén usando.
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ROT13));
+            this.buttonIniciar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbEncriptado = new System.Windows.Forms.Label();
+            this.lbDesencriptado = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lbTXTDesencriptado = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbvl = new System.Windows.Forms.Label();
+            this.lbRot13 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonIniciar
+            // 
+            this.buttonIniciar.BackColor = System.Drawing.Color.Lime;
+            this.buttonIniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonIniciar.Location = new System.Drawing.Point(313, 164);
+            this.buttonIniciar.Name = "buttonIniciar";
+            this.buttonIniciar.Size = new System.Drawing.Size(146, 28);
+            this.buttonIniciar.TabIndex = 0;
+            this.buttonIniciar.Text = "INICIAR";
+            this.buttonIniciar.UseVisualStyleBackColor = false;
+            this.buttonIniciar.Click += new System.EventHandler(this.buttonIniciar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(287, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Encriptado por ROT13";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(222, 121);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(349, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lbEncriptado
+            // 
+            this.lbEncriptado.AutoSize = true;
+            this.lbEncriptado.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lbEncriptado.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.lbEncriptado.Location = new System.Drawing.Point(61, 388);
+            this.lbEncriptado.Name = "lbEncriptado";
+            this.lbEncriptado.Size = new System.Drawing.Size(0, 16);
+            this.lbEncriptado.TabIndex = 4;
+            // 
+            // lbDesencriptado
+            // 
+            this.lbDesencriptado.AutoSize = true;
+            this.lbDesencriptado.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lbDesencriptado.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.lbDesencriptado.Location = new System.Drawing.Point(479, 278);
+            this.lbDesencriptado.Name = "lbDesencriptado";
+            this.lbDesencriptado.Size = new System.Drawing.Size(0, 16);
+            this.lbDesencriptado.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label5.Location = new System.Drawing.Point(3, 253);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(135, 16);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Encriptado ROT13";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label6.Location = new System.Drawing.Point(3, 360);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 16);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Encriptado";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label7.Location = new System.Drawing.Point(421, 252);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(110, 16);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Desencriptado";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label8.Location = new System.Drawing.Point(421, 321);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(151, 16);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Texto desencriptado";
+            // 
+            // lbTXTDesencriptado
+            // 
+            this.lbTXTDesencriptado.AutoSize = true;
+            this.lbTXTDesencriptado.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lbTXTDesencriptado.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.lbTXTDesencriptado.Location = new System.Drawing.Point(482, 352);
+            this.lbTXTDesencriptado.Name = "lbTXTDesencriptado";
+            this.lbTXTDesencriptado.Size = new System.Drawing.Size(0, 16);
+            this.lbTXTDesencriptado.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label2.Location = new System.Drawing.Point(3, 308);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(240, 16);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Valores asignados para encriptar";
+            // 
+            // lbvl
+            // 
+            this.lbvl.AutoSize = true;
+            this.lbvl.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lbvl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.lbvl.Location = new System.Drawing.Point(61, 333);
+            this.lbvl.Name = "lbvl";
+            this.lbvl.Size = new System.Drawing.Size(0, 16);
+            this.lbvl.TabIndex = 12;
+            // 
+            // lbRot13
+            // 
+            this.lbRot13.AutoSize = true;
+            this.lbRot13.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lbRot13.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.lbRot13.Location = new System.Drawing.Point(64, 270);
+            this.lbRot13.Name = "lbRot13";
+            this.lbRot13.Size = new System.Drawing.Size(0, 16);
+            this.lbRot13.TabIndex = 13;
+            // 
+            // ROT13
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbRot13);
+            this.Controls.Add(this.lbvl);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbTXTDesencriptado);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbDesencriptado);
+            this.Controls.Add(this.lbEncriptado);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonIniciar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ROT13";
+            this.Text = "ROT13";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonIniciar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbEncriptado;
+        private System.Windows.Forms.Label lbDesencriptado;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lbTXTDesencriptado;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbvl;
+        private System.Windows.Forms.Label lbRot13;
+    }
+}
+
---
+++ ROT13/Form1.cs
@@ -0,0 +1,464 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ROT13
+{
+    public partial class ROT13 : Form
+    {
+        private int a = 1, b = 2, c = 3, d = 4, e = 5, f = 6, g = 7, h = 8, i = 9, j = 10, k = 11, l = 12, m = 13, n = 14, o = 15, p = 16, q = 17, r = 18, s = 19, t = 20, u = 21, v = 22, w = 23, x = 24, y = 25, z = 26;
+
+        private void lbROT13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public ROT13()
+        {
+            InitializeComponent();
+            buttonIniciar.BackColor = Color.Blue;
+            //Se coloca en color rojo el boton iniciar
+            textBox1.CharacterCasing = CharacterCasing.Lower;
+        }
+        public void SeEncripta(String entrada)
+        {
+            Char caracter;
+            //Se inicia un siclo para la verificacion de la operacion sea correcta
+            char[] encriptar = new char[entrada.Length];
+
+
+            for (int ii = 0; ii < entrada.Length; ii++)
+            {
+                caracter = entrada.ElementAt(ii);
+
+                switch (caracter)
+                {
+                    case 'a':
+                        encriptar[ii] = 'n';
+                        break;
+                    case 'b':
+                        encriptar[ii] = 'o';
+                        break;
+                    case 'c':
+                        encriptar[ii] = 'p';
+                        break;
+                    case 'd':
+                        encriptar[ii] = 'q';
+                        break;
+                    case 'e':
+                        encriptar[ii] = 'r';
+                        break;
+                    case 'f':
+                        encriptar[ii] = 's';
+                        break;
+                    case 'g':
+                        encriptar[ii] = 't';
+                        break;
+                    case 'h':
+                        encriptar[ii] = 'u';
+                        break;
+                    case 'i':
+                        encriptar[ii] = 'v';
+                        break;
+                    case 'j':
+                        encriptar[ii] = 'w';
+                        break;
+                    case 'k':
+                        encriptar[ii] = 'x';
+                        break;
+                    case 'l':
+                        encriptar[ii] = 'y';
+                        break;
+                    case 'm':
+                        encriptar[ii] = 'z';
+                        break;
+                    case 'n':
+                        encriptar[ii] = 'a';
+                        break;
+                    case 'o':
+                        encriptar[ii] = 'b';
+                        break;
+                    case 'p':
+                        encriptar[ii] = 'c';
+                        break;
+                    case 'q':
+                        encriptar[ii] = 'd';
+                        break;
+                    case 'r':
+                        encriptar[ii] = 'e';
+                        break;
+                    case 's':
+                        encriptar[ii] = 'f';
+                        break;
+                    case 't':
+                        encriptar[ii] = 'g';
+                        break;
+                    case 'u':
+                        encriptar[ii] = 'h';
+                        break;
+                    case 'v':
+                        encriptar[ii] = 'i';
+                        break;
+                    case 'w':
+                        encriptar[ii] = 'j';
+                        break;
+                    case 'x':
+                        encriptar[ii] = 'k';
+                        break;
+                    case 'y':
+                        encriptar[ii] = 'l';
+                        break;
+                    case 'z':
+                        encriptar[ii] = 'm';
+                        break;
+                    default:
+                        encriptar[ii] = ' ';
+                        break;
+                }
+
+            }
+            String primer = " ";
+            Console.WriteLine("Encriptado por ROT13 \n \n");
+            for (int i = 0; i < entrada.Length; i++)
+            {
+                primer += encriptar[i];
+                Console.Write(encriptar[i]);
+            }
+            lbRot13.Text = primer;
+            Console.WriteLine();
+            Char letra;
+            int[] encripto = new int[entrada.Length];
+            for (int ii = 0; ii < entrada.Length; ii++)
+            {
+                // se declara cada elemento del string para extrar con elementAt
+                letra = entrada.ElementAt(ii);
+                switch (letra)
+                {
+                    case 'a':
+                        encripto[ii] = a;
+                        break;
+                    case 'b':
+                        encripto[ii] = b;
+                        break;
+                    case 'c':
+                        encripto[ii] = c;
+                        break;
+                    case 'd':
+                        encripto[ii] = d;
+                        break;
+                    case 'e':
+                        encripto[ii] = e;
+                        break;
+                    case 'f':
+                        encripto[ii] = f;
+                        break;
+                    case 'g':
+                        encripto[ii] = g;
+                        break;
+                    case 'h':
+                        encripto[ii] = h;
+                        break;
+                    case 'i':
+                        encripto[ii] = i;
+                        break;
+                    case 'j':
+                        encripto[ii] = j;
+                        break;
+                    case 'k':
+                        encripto[ii] = k;
+                        break;
+                    case 'l':
+                        encripto[ii] = l;
+                        break;
+                    case 'm':
+                        encripto[ii] = m;
+                        break;
+                    case 'n':
+                        encripto[ii] = n;
+                        break;
+                    case 'o':
+                        encripto[ii] = o;
+                        break;
+                    case 'p':
+                        encripto[ii] = p;
+                        break;
+                    case 'q':
+                        encripto[ii] = q;
+                        break;
+                    case 'r':
+                        encripto[ii] = r;
+                        break;
+                    case 's':
+                        encripto[ii] = s;
+                        break;
+                    case 't':
+                        encripto[ii] = t;
+                        break;
+                    case 'u':
+                        encripto[ii] = u;
+                        break;
+                    case 'v':
+                        encripto[ii] = v;
+                        break;
+                    case 'w':
+                        encripto[ii] = w;
+                        break;
+                    case 'x':
+                        encripto[ii] = x;
+                        break;
+                    case 'y':
+                        encripto[ii] = y;
+                        break;
+                    case 'z':
+                        encripto[ii] = z;
+                        break;
+                    default:
+                        encripto[ii] = ' ';
+                        break;
+                }
+            }
+            String segundo = " ";
+            Console.WriteLine("Encripta");
+            for (int ii = 0; ii < entrada.Length; ii++)
+            {
+                segundo += encripto[ii];
+                Console.WriteLine(encripto[ii]);
+            }
+            Console.WriteLine();
+            lbvl.Text = segundo;
+            int[] encriptado = new int[entrada.Length];
+
+            int contador = 1;
+
+            int[] p1 = new int[3];
+            int[] p2 = new int[3];
+            int[] p3 = new int[3];
+
+            //Se encripta
+            for (int ii = 0; ii < entrada.Length; ii++)
+            {
+                if (contador == 1)
+                {
+                    p1[0] = encripto[ii] * 1;
+                    p2[0] = encripto[ii] * 2;
+                    p3[0] = encripto[ii] * 1;
+                }
+                else if (contador == 2)
+                {
+                    p1[1] = encripto[ii] * 0;
+                    p2[1] = encripto[ii] * (-1);
+                    p3[1] = encripto[ii] * 3;
+                }
+                else if (contador == 3)
+                {
+                    p1[2] = encripto[ii] * 2;
+                    p2[2] = encripto[ii] * 1;
+                    p3[2] = encripto[ii] * 0;
+
+                    encriptado[ii - 2] = p1[0] + p1[1] + p1[2];
+                    encriptado[ii - 1] = p2[0] + p2[1] + p2[2];
+                    encriptado[ii] = p3[0] + p3[1] + p3[2];
+
+                    contador = 0;
+                    Console.WriteLine();
+                }
+                contador += 1;
+            }
+            //Imprimo resultados encriptados
+            String tercero = "";
+            Console.WriteLine("Valores encriptados");
+            for (int ii = 0; ii < entrada.Length; ii++)
+            {
+                tercero += encriptado[ii];
+                Console.WriteLine(encriptado[ii]);
+            }
+            lbEncriptado.Text = tercero;
+            //Desencriptar
+
+            int[] Desencriptar = new int[entrada.Length];
+
+            for (int ii = 0; ii < entrada.Length; ii++)
+            {
+                if (contador == 1)
+                {
+                    p1[0] = encriptado[ii] * 1;
+                    p2[0] = encriptado[ii] * 2;
+                    p3[0] = encriptado[ii] * 1;
+                }
+                else if (contador == 2)
+                {
+                    p1[1] = encriptado[ii] * 0;
+                    p2[1] = encriptado[ii] * (-1);
+                    p3[1] = encriptado[ii] * 3;
+                }
+                else if (contador == 3)
+                {
+                    p1[2] = encriptado[ii] * 2;
+                    p2[2] = encriptado[ii] * 1;
+                    p3[2] = encriptado[ii] * 0;
+
+                    Desencriptar[ii] = p3[0] + p3[1] + p3[2];
+
+
+                    contador = 0;
+                    Console.WriteLine();
+                }
+                contador += 1;
+            }
+            String cuarto = "";
+            //Imprimo resultados Desencriptados
+            Console.WriteLine("Valores desencriptados");
+            for (int ii = 0; ii < entrada.Length; ii++)
+            {
+                cuarto += encripto[ii];
+                Console.WriteLine(encripto[ii]);
+            }
+            Console.WriteLine();
+            lbDesencriptado.Text = cuarto;
+            //Desencriptar
+            char[] palabra = new char[encripto.Length];
+            for (int ii = 0; ii < encripto.Length; ii++)
+            {
+                // se declara cada elemento del string para extrar con elementAt
+                int uso = encripto[ii];
+
+                switch (uso)
+                {
+                    case 1:
+                        palabra[ii] = 'a';
+                        break;
+                    case 2:
+                        palabra[ii] = 'b';
+                        break;
+                    case 3:
+                        palabra[ii] = 'c';
+                        break;
+                    case 4:
+                        palabra[ii] = 'd';
+                        break;
+                    case 5:
+                        palabra[ii] = 'e';
+                        break;
+                    case 6:
+                        palabra[ii] = 'f';
+                        break;
+                    case 7:
+                        palabra[ii] = 'g';
+                        break;
+                    case 8:
+                        palabra[ii] = 'h';
+                        break;
+                    case 9:
+                        palabra[ii] = 'i';
+                        break;
+                    case 10:
+                        palabra[ii] = 'j';
+                        break;
+                    case 11:
+                        palabra[ii] = 'k';
+                        break;
+                    case 12:
+                        palabra[ii] = 'l';
+                        break;
+                    case 13:
+                        palabra[ii] = 'm';
+                        break;
+                    case 14:
+                        palabra[ii] = 'n';
+                        break;
+                    case 15:
+                        palabra[ii] = 'o';
+                        break;
+                    case 16:
+                        palabra[ii] = 'p';
+                        break;
+                    case 17:
+                        palabra[ii] = 'q';
+                        break;
+                    case 18:
+                        palabra[ii] = 'r';
+                        break;
+                    case 19:
+                        palabra[ii] = 's';
+                        break;
+                    case 20:
+                        palabra[ii] = 't';
+                        break;
+                    case 21:
+                        palabra[ii] = 'u';
+                        break;
+                    case 22:
+                        palabra[ii] = 'v';
+                        break;
+                    case 23:
+                        palabra[ii] = 'w';
+                        break;
+                    case 24:
+                        palabra[ii] = 'x';
+                        break;
+                    case 25:
+                        palabra[ii] = 'y';
+                        break;
+                    case 26:
+                        palabra[ii] = 'z';
+                        break;
+                    default:
+                        palabra[ii] = ' ';
+                        break;
+                }
+            }
+            String quito = "";
+            //Valores desencriptados
+            Console.WriteLine("Valores regresados a la normalidad");
+            for (int ii = 0; ii < encripto.Length; ii++)
+            {
+                quito += palabra[ii];
+                Console.WriteLine(palabra[ii]);
+            }
+            Console.WriteLine();
+            lbTXTDesencriptado.Text = quito;
+        }
+        private void buttonIniciar_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text == "")
+                //Valido que el campo de texto este vacio
+            {
+                MessageBox.Show("Operacion Mal", "¡UPS!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            //Si no es vacio se inicia el analizador
+            {
+                Console.WriteLine("- INGRESO -");
+                String entrada = textBox1.Text;
+                //Decalro el texto ingresado en un string
+                SeEncripta(entrada);
+                buttonIniciar.BackColor = Color.Violet;
+                //Se coloca en color rojo el boton iniciar
+                Console.WriteLine(" - SALIO -");
+
+                buttonIniciar.BackColor = Color.Red;
+                //Se coloca en color rojo el boton iniciar
+
+            }
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
